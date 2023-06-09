10f525d3160c56c8c8c096dc502f25150bc01e17
yanorei32 <yanorei@hotmail.co.jp>
2023-06-09 16:50:56 +0900

    Update Example

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Rect::new(Size::new(640, 384), image::Rgb([192, 192, 192]))
         .overlay(
             Point::new(20, 20),
-            ScaledTextArea::new(b"2023/06/09 (WED) BIN", 2).remap(|v| match v {
-                Cutout::Opaque(_) => Cutout::Opaque(image::Rgb([0, 0, 0])),
-                Cutout::Cutout => Cutout::Cutout,
-            }),
-        )
-        .unwrap()
-        .overlay(
-            Point::new(20, 80),
-            ScaledTextArea::new(b"Error: Failed to read SD Card.", 2).remap(|v| match v {
+            ScaledTextArea::new(b"2023/06/09 (WED)", 2).remap(|v| match v {
                 Cutout::Opaque(_) => Cutout::Opaque(image::Rgb([0, 0, 0])),
                 Cutout::Cutout => Cutout::Cutout,
             }),
